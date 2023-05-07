f9a2aacc28a52bd3ecee17d17f3cec6f4ce3d22b
zyadmedhat1223 <zyadcool77@gmail.com>
2023-05-07 14:56:36 +0300

    Revert "Revert "Merge branch 'main' of https://github.com/lolix057/pharmacy""
    This reverts commit 9aed26b810cd1af0e3957aa875a94faa1753b8c9.

---
+++ payment.js
@@ -0,0 +1,21 @@
+$(function () {
+    $('[data-toggle="popover"]').popover();
+    
+    $('#cvc').on('click', function(){
+      if ( $('.cvc-preview-container').hasClass('hide') ) {
+        $('.cvc-preview-container').removeClass('hide');
+      } else {
+        $('.cvc-preview-container').addClass('hide');
+      }    
+    });
+    
+});
+function sendPaymentAlert() {
+    alert("The payment has successfully done.");
+}
+  
+
+
+var payButton = document.getElementById("PayButton");
+payButton.addEventListener("click", sendPaymentAlert);
+  
