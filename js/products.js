94049e436b1cd2027ef4c237546670daf739c80c
Amira Ahmed <hashemamira@69gmail.com>
2023-05-12 21:28:44 +0200

    amira

--- js/products.js
+++ js/products.js
@@ -20,3 +20,60 @@ previewBox.forEach(close =>{
     preveiwContainer.style.display = 'none';
   };
 });
+
+/*-------------department1--------------*/
+function closeWindow_Headache(){
+  window.location.href="/Headache.html";
+}
+function closeWindow_AllergySinus(){
+  window.location.href="/Allergy&Sinus.html";
+}
+
+function closeWindow_backjoint(){
+  window.location.href="/backjoint.html";
+}
+function closeWindow_Constipation(){
+  window.location.href="/Constipation.html";
+}
+
+function closeWindow_Cough(){
+  window.location.href="/Cough.html";
+}
+function closeWindow_Diarrhea(){
+  window.location.href="/Diarrhea.html";
+}
+function closeWindow_Fever(){
+  window.location.href="/Fever.html";
+}
+function closeWindow_Vomiting(){
+  window.location.href="/Vomiting.html";
+}
+
+
+/*-------------department2--------------*/
+
+
+function closeWindow_Baby_and_Mom(){
+  window.location.href="/Baby_and_Mom.html";
+}
+function closeWindow_EyeAndEarCare(){
+  window.location.href="/Eye and Ear Care.html";
+}
+
+function closeWindow_HairCare(){
+  window.location.href="/Hair Care.html";
+}
+function closeWindow_OralCare(){
+  window.location.href="/Oral Care.html";
+}
+
+function closeWindow_PersonalCare(){
+  window.location.href="/Personal Care.html";
+}
+function closeWindow_SkinCare(){
+  window.location.href="/Skin Care.html";
+}
+
+function closeWindow_VitaminAndSupplements(){
+  window.location.href="/Vitamin and Supplements.html";
+}
